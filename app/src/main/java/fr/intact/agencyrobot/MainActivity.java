37d2e4b973841211e6c15df0cfddd1407b51afa5
wferreira <william.ferreira@in-tact.fr>
2020-05-01 23:23:21 +0200

    connect to Arduino Agency Robot and control driving

--- app/src/main/java/fr/intact/agencyrobot/MainActivity.java
+++ app/src/main/java/fr/intact/agencyrobot/MainActivity.java
@@ -3,38 +3,95 @@ package fr.intact.agencyrobot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import fr.intact.agencyrobot.service.SerialService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SerialService serialService;
+
+    @BindView(R.id.connection_status) TextView connectionStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        serialService = new SerialService(this);
+
         ButterKnife.bind(this);
     }
 
-    @OnClick(R.id.left_arrow) void leftArrowClicked() {
-        Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+    @OnClick(R.id.connect) void connectSerial()  {
+        try {
+            if (serialService.connect()){
+                connectionStatus.setText("Connected");
+            }else{
+                connectionStatus.setText("Disconnected");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @OnClick(R.id.top_arrow) void topArrowClicked() {
-        Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+    @OnClick(R.id.disconnect) void disconnectSerial()  {
+        try {
+            serialService.disconnect();
+            connectionStatus.setText("Disconnected");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @OnClick(R.id.right_arrow) void rightArrowClicked() {
+    @OnClick(R.id.left_arrow) void leftArrowClicked()  {
         Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+        try {
+            serialService.write("L");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @OnClick(R.id.bottom_arrow) void bottomArrowClicked() {
+    @OnClick(R.id.top_arrow) void topArrowClicked()  {
         Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+        try {
+            serialService.write("F");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @OnClick(R.id.stop_arrow) void stopClicked() {
+    @OnClick(R.id.right_arrow) void rightArrowClicked()  {
         Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+        try {
+            serialService.write("R");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick(R.id.bottom_arrow) void bottomArrowClicked()  {
+        Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+        try {
+            serialService.write("B");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @OnClick(R.id.stop_arrow) void stopClicked()  {
+        Toast.makeText(getApplicationContext(), R.string.app_name, Toast.LENGTH_LONG).show();
+        try {
+            serialService.write("S");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/fr/intact/agencyrobot/service/SerialService.java
@@ -0,0 +1,65 @@
+package fr.intact.agencyrobot.service;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
+import java.io.IOException;
+import java.util.List;
+
+import fr.intact.agencyrobot.BuildConfig;
+
+public class SerialService {
+
+    static final String INTENT_ACTION_GRANT_USB = BuildConfig.APPLICATION_ID + ".GRANT_USB";
+    private static final int WRITE_WAIT_MILLIS = 2000;
+
+    private Activity activity;
+
+    private UsbSerialPort port;
+
+    public SerialService(Activity activity){
+        this.activity = activity;
+
+    }
+
+    public boolean connect() throws IOException {
+        // Find all available drivers from attached devices.
+        UsbManager manager = (UsbManager) activity.getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            return false;
+        }
+
+        // Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+
+            PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(activity, 0, new Intent(INTENT_ACTION_GRANT_USB), 0);
+            manager.requestPermission(driver.getDevice(), usbPermissionIntent);
+            return false;
+        }
+
+        port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        port.open(connection);
+        port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+
+        return true;
+    }
+
+    public void write(String command) throws IOException {
+        port.write(command.getBytes(), WRITE_WAIT_MILLIS);
+    }
+
+    public void disconnect() throws IOException {
+        port.close();
+    }
+}
